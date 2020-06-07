be213ab589e5a197bd27c03f1b2528352681f9f7
aknimbal <48735789+aknimbal@users.noreply.github.com>
2020-06-07 17:58:46 -0400

    Add files via upload

---
+++ deploy.ps1
@@ -0,0 +1,22 @@
+#Requires -Version 3.0
+
+Connect-AzureRmAccount
+
+# Variables
+#-----------------------------------------------------------
+$ResourceGroupName = "inspirepocrg"
+$ResourceGroupLocation = "eastus"
+
+# Create the resource group only when it doesn't already exist
+
+Write-Output '', 'Checking and creating resourcess needed for setup'
+
+if ((Get-AzureRmResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation -Verbose -ErrorAction SilentlyContinue) -eq $null) {
+    Write-Output '', 'Creating Resource Group - ', @($ResourceGroupName)
+    New-AzureRmResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation -Verbose -Force -ErrorAction Stop
+    Write-Output '', @($ResourceGroupName), ' Resource Group created' 	
+}
+
+Write-Output '', 'Starting ARM deployment of services. This might take a while'
+
+New-AzureRMResourceGroupDeployment -ResourceGroupName aksprivatedemo-rg -TemplateFile ./azuredeploy.json -TemplateParameterFile ./azuredeploy.parameters.json
